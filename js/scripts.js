052e88a0e5536b7482606caabdeca2b398ecd1f0
Welthanos <welthanos_dev@outlook.com>
2024-01-12 07:09:13 -0300

    feat: exercise 01

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,11 @@
 // EX 01
+const orderedList = () => {
+    const list = [12, 14, 94, 34, 30, 35, 57, 24, 19, 10];
+    const text = document.getElementById('ordered-list');
+
+    text.innerText = list.sort().join(' ');
+    text.style.display = 'inline';
+}
 
 // EX 02
 
