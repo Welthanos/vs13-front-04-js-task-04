5d93aa8280188fb03863d9a9d5548be0b001fdb2
Welthanos <welthanos_dev@outlook.com>
2024-01-12 09:15:13 -0300

    refactor: exercise 1 to 3

--- js/scripts.js
+++ js/scripts.js
@@ -2,19 +2,20 @@
 const printOrderedList = () => {
     const list = [12, 14, 94, 34, 30, 35, 57, 24, 19, 10];
     const text = document.getElementById('ordered-list');
+    const btn = document.getElementById('btn-section-1');
 
-    text.innerText = list.sort().join(' ');
-    text.style.display = 'inline';
+    text.innerHTML += `<br>Lista ordenada:<br>[${list.sort().join(', ')}]`;
+    btn.disabled = true;
 }
 
 // EX 02
 const printEvenAndIndex = () => {
     const text = document.getElementById('even-list');
     const btn = document.getElementById('btn-section-2');
-
     const numbers = [];
 
     for (let i = 1; i <= 30; i++) numbers.push(i);
+
     text.style.display = 'inline';
 
     for (number of numbers) if (number % 2 === 0) text.innerText += `O número par é ${number} e seu índice é ${numbers.indexOf(number)}.\n`;
@@ -28,13 +29,11 @@ const alterNumbersOfList = () => {
     const list = [1.5, 6, 7.8, 19, 45, 89, 50.5, 25];
     const indexToAlter = [0, 2, 5];
 
-    for (index of indexToAlter) list.splice(index, 1, (Math.random() * 100).toFixed(1));
+    for (index of indexToAlter) list.splice(index, 1, (Math.random() * 50).toFixed(1));
 
     text.innerHTML += `<br>Lista alterada:<br>[${list.join(', ')}]`;
     btn.disabled = true;
 }
-
-
 
 // EX 04
 
